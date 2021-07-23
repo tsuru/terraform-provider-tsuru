341364b8a7296835ff594805d1534423851396d6
Wilson Júnior <wilsonpjunior@gmail.com>
2021-07-23 09:31:25 -0300

    Update outdated examples

--- example/main.tf
+++ example/main.tf
@@ -15,16 +15,16 @@ resource "tsuru_app" "my-app" {
 
   description = "app created with terraform"
 
-  plan = "c0.1m0.2"
-  pool = "staging"
+  plan     = "c0.1m0.2"
+  pool     = "staging"
   platform = "python"
 
   team_owner = "admin"
 
-  tags = [ "a", "b" ]
+  tags = ["a", "b"]
   metadata {
     labels = {
-      "label1" = "1"
+      "label1"     = "1"
       "io.tsuru/a" = "2"
     }
     annotations = {
@@ -36,22 +36,22 @@ resource "tsuru_app" "my-app" {
 }
 
 resource "tsuru_app_grant" "app-permissions-team-a" {
-  app = tsuru_app.my-app.name
+  app  = tsuru_app.my-app.name
   team = "team-a"
 }
 
 resource "tsuru_app_grant" "app-permissions-team-b" {
-  app = tsuru_app.my-app.name
+  app  = tsuru_app.my-app.name
   team = "team-b"
 }
 
 resource "tsuru_app_cname" "app-extra-cname" {
-  app = tsuru_app.my-app.name
+  app      = tsuru_app.my-app.name
   hostname = "sample.tsuru.i.mycompany.com"
 }
 
 resource "tsuru_app_router" "other-router" {
-  app = tsuru_app.my-app.name
+  app  = tsuru_app.my-app.name
   name = "internal-http-lb"
   options = {
     "key" = "value"
@@ -59,29 +59,23 @@ resource "tsuru_app_router" "other-router" {
 }
 
 resource "tsuru_app_autoscale" "web" {
-  app = tsuru_app.my-app.name
-  process = "web"
-  min_units = 3
-  max_units = 10
+  app         = tsuru_app.my-app.name
+  process     = "web"
+  min_units   = 3
+  max_units   = 10
   cpu_average = "800m"
 }
 
 resource "tsuru_app_env" "env" {
-  app = tsuru_app.my-app.name
+  app               = tsuru_app.my-app.name
   restart_on_update = false
 
-  environment_variable {
-    name = "env1"
-    value = "10"
+  environment_variables = {
+    "ENV1" = "10"
   }
 
-  environment_variable {
-    name = "env2"
-    sensitive_value = "12"
-  }
-
-  environment_variable {
-    name = "env3"
-    value = "12"
+  private_environment_variables = {
+    "ENV2" = "12"
+    "ENV3" = "12"
   }
 }
