d6d2c2222a29ce106cf2042650e13a811e13a8c3
Wilson Júnior <wilsonpjunior@gmail.com>
2024-04-11 11:26:28 -0300

    Update example of tsuru_app_autoscale

--- examples/resources/tsuru_app_autoscale/resource.tf
+++ examples/resources/tsuru_app_autoscale/resource.tf
@@ -7,15 +7,22 @@ resource "tsuru_app_autoscale" "web" {
 
   schedule {
     min_replicas = 5
-    start = "0 18 * * *"
-    end = "0 0 * * *"
-    timezone = "America/Sao_Paulo"
+    start        = "0 18 * * *"
+    end          = "0 0 * * *"
+    timezone     = "America/Sao_Paulo"
   }
 
   schedule {
     min_replicas = 10
-    start = "0 18 * * *"
-    end = "0 19 * * *"
-    timezone = "UTC"
+    start        = "0 18 * * *"
+    end          = "0 19 * * *"
+    timezone     = "UTC"
+  }
+
+  prometheus {
+    name           = "metric-test"
+    threshold      = 2.7
+    query          = "sum(rate(test_requests_total[1m])) * 60"
+    custom_address = "http://prometheus.example.com:9090"
   }
 }
