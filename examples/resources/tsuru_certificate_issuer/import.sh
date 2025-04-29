722c31a7c3e679bb0cad835ead0c6a0dd127a7c9
crgisch <claudio.gisch@hotmail.com>
2025-04-29 14:41:27 -0300

    docs: update examples

--- examples/resources/tsuru_certificate_issuer/import.sh
+++ examples/resources/tsuru_certificate_issuer/import.sh
@@ -1,8 +1,4 @@
 terraform import tsuru_certificate_issuer.resource_name "app::cname::issuer"
-# or
-terraform import tsuru_certificate_issuer.resource_name "app::cname"
 
 # example
 terraform import tsuru_certificate_issuer.cert "my-app::my-domain.com::letsencrypt"
-# or just (issuer will be discovered automatically)
-terraform import tsuru_certificate_issuer.cert "my-app::my-domain.com"
